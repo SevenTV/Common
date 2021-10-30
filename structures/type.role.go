c8627b85fa6efc075f20e45ee5df3089911c0766
Anatole <me@anatole.dev>
2021-10-30 15:20:29 +0200

    Initial permission bits

--- structures/type.role.go
+++ structures/type.role.go
@@ -31,20 +31,38 @@ func (r *Role) HasPermissionBit(bit RolePermission) bool {
 // RolePermission: Role permission bits
 type RolePermission int64
 
+// Emotes
+// Range: 1 << 1 - 1 << 12
 const (
-	// Emotes
-	// Range: 1 << 1 - 1 << 8
-	RolePermissionEmotesCreate int64 = 1 << iota // 1 - Allows creating emotes
-	RolePermissionEmotesEdit                     // 2 - Allows editing owned emotes
+	RolePermissionCreateEmote     RolePermission = 1 << 0 // 1 - Allows creating emotes
+	RolePermissionEditEmote       RolePermission = 1 << 1 // 2 - Allows editing / creating new versions of an emote
+	RolePermissionSetChannelEmote RolePermission = 1 << 2 // 4 - Allows adding or removing channel emotes
+)
 
-	// Reporting
-	// Range: 1 << 9- 1 << 12
+// User / Misc / Special
+// Range: 1 << 13 - 1 << 1 << 29
+const (
+	RolePermissionReportCreate RolePermission = 1 << 13 // 8192 - Allows creating reports
 
-	// Moderation
-	// Range: 1 << 13 - 1 << 24
+	RolePermissionUseZeroWidthEmoteType RolePermission = 1 << 23 // 8388608 - Allows using the Zero-Width emote type
+	RolePermissionAnimateProfilePicture RolePermission = 1 << 24 // 16777216 - Allows the user's profile picture to be animated
+)
 
-	// Administration
-	// Range: 1 << 25 - 1 << 32
+// Moderation
+// Range: 1 << 30 - 1 << 53
+const (
+	RolePermissionManageBans      RolePermission = 1 << 30 // 1073741824 - (Mod) Allows creating or deleting bans
+	RolePermissionManageRoles     RolePermission = 1 << 31 // 2147483648 - (Mod) Allows creating, deleting and assigning roles to users
+	RolePermissionEditAnyEmote    RolePermission = 1 << 32 // 4294967296 - (Mod) Allows editing any emote
+	RolePermissionEditAnyEmoteSet RolePermission = 1 << 33 // 8589934592 - (Mod) Allows editing any emote set, unless it is a privileged set
+)
+
+// Administration
+// Range: 1 << 54 - 1 << 62
+const (
+	RolePermissionSuperAdministrator RolePermission = 1 << 62 // 4611686018427387904 - (Admin) GRANTS EVERY PERMISSION /!\
+	RolePermissionManageNews         RolePermission = 1 << 54 // 18014398509481984 - (Admin) Allows creating and editing news
+	RolePermissionManageStack        RolePermission = 1 << 55 // 36028797018963968 - (Admin) Allows managing the application stack
 )
 
 type RoleBuilder struct {
