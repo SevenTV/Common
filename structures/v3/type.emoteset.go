75c90bc0eca2f9d172a98dda8afcbd3180789146
Anatole <me@anatole.dev>
2022-01-14 10:21:53 +0100

    Finalize EmoteSet structure

--- structures/v3/type.emoteset.go
+++ structures/v3/type.emoteset.go
@@ -1,15 +1,20 @@
 package structures
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type EmoteSet struct {
 	ID primitive.ObjectID `json:"-" bson:"_id,omitempty"`
 	// Numeric unique ID for the set
 	// Starts from 1 and increments per set created
-	NumericID int32 `json:"id" bson:"num_id"`
+	NumericID uint64 `json:"id" bson:"num_id"`
 	// A unique tag for the set, between 2 and 5 characters (letters and numbers only)
 	// This can be used for targeting and parsing
-	Tag string `json:"tag" bson:"tag"`
+	// May also be an empty string, meaning no tag.
+	Tag string `json:"tag,omitempty" bson:"tag,omitempty"`
 	// Whether or not the emote set can be edited
 	Immutable bool `json:"immutable" bson:"immutable"`
 	// If true, the set is "privileged" and can only be modified by its owner or a super administrator, regardless of the "Edit Any Emote Set" permission
@@ -19,8 +24,8 @@ type EmoteSet struct {
 	// The emotes assigned to this set
 	Emotes []*ActiveEmote `json:"emotes" bson:"emotes"`
 	// The maximum amount of emotes this set is allowed to contain
-	EmoteSlots int32 `json:"emote_slots" bson:"emote_slots"`
-	// The ID of the user who owns this emote
+	EmoteSlots uint32 `json:"emote_slots" bson:"emote_slots"`
+	// The ID of the user who owns this emote set
 	OwnerID primitive.ObjectID `json:"owner_id" bson:"owner_id"`
 
 	// TODO: filters, i.e allow set to be used only in specific channels
@@ -31,8 +36,9 @@ type EmoteSet struct {
 }
 
 type ActiveEmote struct {
-	ID    primitive.ObjectID `json:"id" bson:"id"`
-	Alias string             `json:"alias,omitempty" bson:"alias,omitempty"`
+	ID        primitive.ObjectID `json:"id" bson:"id"`
+	Alias     string             `json:"alias,omitempty" bson:"alias,omitempty"`
+	Timestamp time.Time          `json:"timestamp" bson:"timestamp"`
 
 	// Relational
 
@@ -44,7 +50,7 @@ type EmoteSetBuilder struct {
 	EmoteSet *EmoteSet
 }
 
-func (esb *EmoteSetBuilder) SetNumericID(i int32) *EmoteSetBuilder {
+func (esb *EmoteSetBuilder) SetNumericID(i uint64) *EmoteSetBuilder {
 	esb.EmoteSet.NumericID = i
 	esb.Update.Set("num_id", i)
 	return esb
@@ -74,7 +80,7 @@ func (esb *EmoteSetBuilder) SetActive(b bool) *EmoteSetBuilder {
 	return esb
 }
 
-func (esb *EmoteSetBuilder) SetEmoteSlots(slots int32) *EmoteSetBuilder {
+func (esb *EmoteSetBuilder) SetEmoteSlots(slots uint32) *EmoteSetBuilder {
 	esb.EmoteSet.EmoteSlots = slots
 	esb.Update.Set("emote_slots", slots)
 	return esb
@@ -83,5 +89,40 @@ func (esb *EmoteSetBuilder) SetEmoteSlots(slots int32) *EmoteSetBuilder {
 func (esb *EmoteSetBuilder) SetOwnerID(id primitive.ObjectID) *EmoteSetBuilder {
 	esb.EmoteSet.OwnerID = id
 	esb.Update.Set("owner_id", id)
+	return esb
+}
+
+func (esb *EmoteSetBuilder) AddActiveEmote(emote *ActiveEmote) *EmoteSetBuilder {
+	for _, e := range esb.EmoteSet.Emotes {
+		if e.ID == emote.ID {
+			return esb // emote already added.
+		}
+	}
+
+	esb.EmoteSet.Emotes = append(esb.EmoteSet.Emotes, emote)
+	esb.Update.AddToSet("emotes", emote)
+	return esb
+}
+
+func (esb *EmoteSetBuilder) RemoveActiveEmote(emote *ActiveEmote) *EmoteSetBuilder {
+	ind := int32(-1)
+	for i := range esb.EmoteSet.Emotes {
+		if esb.EmoteSet.Emotes[i] == nil {
+			continue
+		}
+		if esb.EmoteSet.Emotes[i].ID != emote.ID {
+			continue
+		}
+		ind = int32(i)
+		break
+	}
+	if ind == -1 {
+		return esb // did not find index
+	}
+
+	copy(esb.EmoteSet.Emotes[ind:], esb.EmoteSet.Emotes[ind+1:])
+	esb.EmoteSet.Emotes[len(esb.EmoteSet.Emotes)-1] = nil
+	esb.EmoteSet.Emotes = esb.EmoteSet.Emotes[:len(esb.EmoteSet.Emotes)-1]
+	esb.Update.Pull("emotes.id", emote.ID)
 	return esb
 }
