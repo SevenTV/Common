05aae09b069eb9ebf81e61ff8ff4d60053350f67
Anatole <me@anatole.dev>
2022-02-01 16:28:30 +0100

    Refactor naming convention on mutations

---
+++ structures/v3/mutations/user.active_emote_set.mutation.go
@@ -0,0 +1,71 @@
+package mutations
+
+import (
+	"context"
+
+	"github.com/SevenTV/Common/errors"
+	"github.com/SevenTV/Common/mongo"
+	"github.com/SevenTV/Common/structures/v3"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func (um *UserMutation) SetActiveEmoteSet(ctx context.Context, inst mongo.Instance, opt SetUserActiveEmoteSet) (*UserMutation, error) {
+	if um.UserBuilder == nil || um.UserBuilder.User == nil {
+		return um, errors.ErrInternalIncompleteMutation()
+	}
+	if opt.EmoteSetID.IsZero() {
+		return um, errors.ErrInternalIncompleteMutation().SetDetail("Missing Emote Set ID")
+	}
+
+	// Check for actor's permission to do this
+	ub := um.UserBuilder
+	actor := opt.Actor
+	victim := ub.User
+	if actor != nil && actor.ID != victim.ID {
+		ok := actor.HasPermission(structures.RolePermissionManageUsers)
+		for _, ed := range victim.Editors {
+			if ed.ID != actor.ID {
+				continue
+			}
+			// actor is editor
+			// actor has permission to modify victim's emotes
+			if ed.HasPermission(structures.UserEditorPermissionModifyEmotes) {
+				ok = true
+			}
+		}
+		if !ok {
+			return um, errors.ErrInsufficientPrivilege().SetDetail("You are not an editor of this user")
+		}
+	}
+
+	// Get the connection
+	conn, ok := ub.GetConnection(opt.Platform, opt.ConnectionID)
+	if !ok {
+		return um, errors.ErrUnknownUserConnection()
+	}
+	conn.SetActiveEmoteSet(opt.EmoteSetID)
+
+	// Update document
+	if err := inst.Collection(mongo.CollectionNameUsers).FindOneAndUpdate(
+		ctx,
+		bson.M{"_id": victim.ID, "connections.id": opt.ConnectionID},
+		conn.Update,
+		options.FindOneAndUpdate().SetReturnDocument(options.After),
+	).Decode(victim); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, errors.ErrUnknownUser().SetDetail("Victim was not found and could not be updated")
+		}
+		return nil, errors.ErrInternalServerError().SetDetail(err.Error())
+	}
+
+	return um, nil
+}
+
+type SetUserActiveEmoteSet struct {
+	EmoteSetID   primitive.ObjectID
+	Platform     structures.UserConnectionPlatform
+	Actor        *structures.User
+	ConnectionID string
+}
