98fc6d6404c3f384514a88c3a138cd8af6df4962
Anatole <me@anatole.dev>
2022-02-14 04:52:39 +0100

    Fix default role fetch helper (handle zero result)

--- structures/v3/query/query.go
+++ structures/v3/query/query.go
@@ -19,21 +19,19 @@ type defaultRoles struct {
 	l sync.Mutex
 }
 
-func (x *defaultRoles) Fetch(ctx context.Context, mngo mongo.Instance, redis redis.Instance) []*structures.Role {
+func (x *defaultRoles) Fetch(ctx context.Context, mngo mongo.Instance, rdis redis.Instance) []*structures.Role {
 	x.l.Lock()
 	defer x.l.Unlock()
 
 	// Find from redis
 	result := []*structures.Role{}
-	key := redis.ComposeKey("v3", "cache:default-roles")
-	s, err := redis.RawClient().Get(ctx, key.String()).Result()
+	key := rdis.ComposeKey("v3", "default-roles")
+	s, err := rdis.RawClient().Get(ctx, key.String()).Result()
 	if s != "" {
 		if err := multierror.Append(err, json.Unmarshal(utils.S2B(s), &result)).ErrorOrNil(); err != nil {
 			logrus.WithError(err).Error("redis, could not unmarshal cached default roles")
 		}
-		if len(result) > 0 { // return result from cache
-			return result
-		}
+		return result
 	}
 
 	// Find from mongo
@@ -47,7 +45,7 @@ func (x *defaultRoles) Fetch(ctx context.Context, mngo mongo.Instance, redis red
 	if err != nil {
 		logrus.WithError(err).Error("could not marshal default roles for redis cache")
 	}
-	if _, err = redis.RawClient().Set(ctx, key.String(), utils.B2S(b), time.Minute*5).Result(); err != nil {
+	if _, err = rdis.RawClient().Set(ctx, key.String(), utils.B2S(b), time.Minute*5).Result(); err != nil {
 		logrus.WithError(err).Error("redis, could not cache default roles")
 	}
 
