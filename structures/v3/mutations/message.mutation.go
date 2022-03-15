7b9e7d42bbdffbe3ecbd545dd81a55820223ddfb
Anatole <me@anatole.dev>
2022-03-15 09:17:53 +0100

    mutation/message - SetReadStates

---
+++ structures/v3/mutations/message.mutation.go
@@ -0,0 +1,109 @@
+package mutations
+
+import (
+	"context"
+
+	"github.com/SevenTV/Common/errors"
+	"github.com/SevenTV/Common/mongo"
+	"github.com/SevenTV/Common/structures/v3"
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func (mm *MessageMutation) SetReadStates(ctx context.Context, inst mongo.Instance, read bool, opt MessageReadStateOptions) (*MessageReadStateResponse, error) {
+	if mm.MessageBuilder == nil || mm.MessageBuilder.Message == nil {
+		return nil, errors.ErrInternalIncompleteMutation()
+	}
+
+	// Check permissions
+	actor := opt.Actor
+	if !opt.SkipPermissionCheck && actor == nil {
+		return nil, errors.ErrUnauthorized()
+	}
+
+	// Find the readstates
+	filter := bson.M{}
+	if len(opt.Filter) > 0 {
+		filter = opt.Filter
+	}
+	filter["message_id"] = mm.MessageBuilder.Message.ID
+	cur, err := inst.Collection(mongo.CollectionNameMessagesRead).Find(ctx, filter)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, errors.ErrUnknownMessage().SetDetail("Couldn't find any read states related to the message")
+		}
+		logrus.WithError(err).Error("mongo, ")
+		return nil, errors.ErrInternalServerError().SetDetail(err.Error())
+	}
+
+	errorList := []error{}
+	w := []mongo.WriteModel{}
+	for cur.Next(ctx) {
+		rs := &structures.MessageRead{}
+		if err := cur.Decode(rs); err != nil {
+			continue
+		}
+		// Can actor do this?
+		if !opt.SkipPermissionCheck && rs.RecipientID != actor.ID {
+			switch rs.Kind {
+			// Check for a mod request
+			case structures.MessageKindModRequest:
+				d := mm.MessageBuilder.DecodeModRequest()
+				errf := errors.Fields{
+					"message_state_id": rs.ID,
+					"msg_kind":         rs.Kind,
+					"target_kind":      d.TargetKind,
+				}
+
+				if d.TargetKind == structures.ObjectKindEmote && !actor.HasPermission(structures.RolePermissionEditAnyEmote) {
+					errorList = append(errorList, errors.ErrInsufficientPrivilege().SetFields(errf))
+					continue // target is emote but actor lacks "edit any emote" permission
+				} else if d.TargetKind == structures.ObjectKindEmoteSet && !actor.HasPermission(structures.RolePermissionEditAnyEmoteSet) {
+					errorList = append(errorList, errors.ErrInsufficientPrivilege().SetFields(errf))
+					continue // target is emote set but actor lacks "edit any emote set" permission
+				} else if d.TargetKind == structures.ObjectKindReport && !actor.HasPermission(structures.RolePermissionManageReports) {
+					errorList = append(errorList, errors.ErrInsufficientPrivilege().SetFields(errf))
+					continue // target is report but actor lacks "manage reports" permission
+				}
+			default:
+				continue
+			}
+		}
+
+		// Add as item to be written
+		w = append(w, &mongo.UpdateOneModel{
+			Filter: bson.M{"_id": rs.ID},
+			Update: bson.M{"$set": bson.M{
+				"read": read,
+			}},
+		})
+	}
+
+	updated := int64(0)
+	if len(w) > 0 {
+		result, err := inst.Collection(mongo.CollectionNameMessagesRead).BulkWrite(ctx, w)
+		if err != nil {
+			logrus.WithError(err).WithField(
+				"message_id", mm.MessageBuilder.Message.ID,
+			).Error("mongo, failed to update message read states")
+			return nil, errors.ErrInternalServerError().SetDetail(err.Error())
+		}
+		updated += result.ModifiedCount
+	}
+
+	return &MessageReadStateResponse{
+		Updated: updated,
+		Errors:  errorList,
+	}, nil
+}
+
+type MessageReadStateOptions struct {
+	Actor               *structures.User
+	Filter              bson.M
+	SkipPermissionCheck bool
+}
+
+type MessageReadStateResponse struct {
+	Updated int64   `json:"changed"`
+	Errors  []error `json:"errors"`
+}
