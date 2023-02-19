8b3b037a12ce413410c97d9447bad994313149ba
Anatole <me@anatole.dev>
2023-02-19 17:54:35 +0100

    type/system: config

--- structures/v3/type.system.go
+++ structures/v3/type.system.go
@@ -5,9 +5,18 @@ type System struct {
 
 	AdminUserID ObjectID `json:"admin_user_id" bson:"admin_user_id"`
 	EmoteSetID  ObjectID `json:"emote_set_id" bson:"emote_set_id"`
+	Config      struct {
+		Extension     SystemConfigExtension `json:"extension" bson:"extension"`
+		ExtensionBeta SystemConfigExtension `json:"extension_beta" bson:"extension_beta"`
+	} `json:"config" bson:"config"`
 }
 
 type SystemDefaultObject struct {
 	ID         ObjectID `json:"id" bson:"id"`
 	Collection string   `json:"collection" bson:"collection"`
+}
+
+type SystemConfigExtension struct {
+	Version   string     `json:"version" bson:"version"`
+	Overrides []struct{} `json:"overrides" bson:"overrides"`
 }
