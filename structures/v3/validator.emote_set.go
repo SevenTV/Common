dbfed5c4ed3ec83af03bd7e1fc91fde8d9a44881
Brian <18603393+brian6932@users.noreply.github.com>
2023-03-12 17:42:51 -0400

    Enable diacritics in emote names and sets

--- structures/v3/validator.emote_set.go
+++ structures/v3/validator.emote_set.go
@@ -10,7 +10,7 @@ type EmoteSetValidator struct {
 	v *EmoteSet
 }
 
-var RegExpEmoteSetName = regexp.MustCompile(`^[a-zA-Z0-9&'_-~# ]{1,40}$`)
+var RegExpEmoteSetName = regexp.MustCompile(`^[A-Za-zÀ-ÖØ-öø-įĴ-őŔ-žǍ-ǰǴ-ǵǸ-țȞ-ȟȤ-ȳɃɆ-ɏḀ-ẞƀ-ƓƗ-ƚƝ-ơƤ-ƥƫ-ưƲ-ƶẠ-ỿ0-9&'_-~# ]{1,40}$`)
 
 func (e *EmoteSet) Validator() EmoteSetValidator {
 	return EmoteSetValidator{e}
